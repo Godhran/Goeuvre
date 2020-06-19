24b8e555435618e086b5c4a0bbc12c89c62e3f02
Odhran Gormley <Godhran@outlook.com>
2020-06-19 20:40:37 +0100

    initial commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/goeuvre'));
+app.use(express.static('./dist/GOEUVRE'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/goeuvre/' }),
+    res.sendFile('index.html', { root: 'dist/GOEUVRE/' }),
 );
 
 app.listen(process.env.PORT || 8080);
