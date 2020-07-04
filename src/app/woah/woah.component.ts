39a70899a28b9005c63bdd5e2d2c7a8e1ddb7091
Odhran Gormley <Godhran@outlook.com>
2020-07-04 20:43:45 +0100

    update

--- src/app/woah/woah.component.ts
+++ src/app/woah/woah.component.ts
@@ -9,7 +9,7 @@ import { TimelineLite, TimelineMax, Linear, Elastic, Expo, Power0 } from 'gsap';
 export class WoahComponent implements OnInit {
 
   ArmAnimation = new TimelineLite({ paused: true, repeat: -1 });
-  rightArmAnimation = new TimelineLite({ paused: true, repeat: -1 });
+  BodyAnimation = new TimelineLite({ paused: true, repeat: -1 });
   HeadAnimation = new TimelineLite({ paused: true, repeat: -1 });
   WoahAnimation = new TimelineLite({ paused: true, repeat: -1 });
 
@@ -21,7 +21,8 @@ export class WoahComponent implements OnInit {
 
   primeAnimations() {
     this.initArms();
-    // this.initHead();
+    this.initHead();
+    this.initBody();
     // this.initRight();
   }
 
@@ -40,40 +41,67 @@ export class WoahComponent implements OnInit {
   // }
 
   initHead(){
-    this.ArmAnimation
+    this.HeadAnimation
     .fromTo('#Mask', 0.5, {
-      transformOrigin: "50% 50%",
-      y:-50,
-      scale:1,
-    }, {
-      y:0,
-      scale:2,
-      ease: Elastic.easeInOut
-    }, "=-0")
-    .fromTo('#Mask', 0.5, {
-      transformOrigin: "50% 50%",
-      y:0,
-      scale:2,
-    }, {
-      y:-50,
-      scale:1,
-      ease: Elastic.easeInOut
-    }, "=-0")
+        transformOrigin: "50% 50%",
+        y:-25,
+        scale:1.025,
+        rotate:-7.5,
+      }, {
+        y:0,
+        scale:1,
+        rotate:7.5,
+        ease: Elastic.easeInOut
+      }, "=-0")   
+      .fromTo('#Mask', 0.5, {
+          transformOrigin: "50% 50%",
+          y:0,
+          scale:1,
+          rotate:7.5,
+        }, {
+          y:-25,
+          scale:1.025,
+          rotate:-7.5,
+        }, "=-0")   
     this.HeadAnimation.play();
+
+  }
+
+  initBody(){
+    this.BodyAnimation
+    .fromTo('#WrestlerEntire', 1, {
+      transformOrigin: "50% 50%",
+      rotate: -7.5,
+      scale:0.9
+    }, {
+      rotate: 7.5,
+      scale:1,
+      ease: Elastic.easeInOut
+    }, "=-0")
+    .fromTo('#WrestlerEntire', 1, {
+      transformOrigin: "50% 50%",
+      rotate: 7.5,
+      scale:1,
+    }, {
+      rotate: -7.5,
+      scale:0.9,
+      ease: Elastic.easeInOut
+    }, "=-0")
+    this.BodyAnimation.play();
 
   }
 
   initArms() {
     this.ArmAnimation
-    .fromTo('#WrestlerEntire', 0.5, {
-      transformOrigin: "50% 50%",
-      rotate: -3.5,
-      scale:0.9
-    }, {
-      rotate: 3.5,
-      scale:1,
-      ease: Elastic.easeInOut
-    }, "=-0")
+    // .fromTo('#WrestlerEntire', 0.5, {
+    //   transformOrigin: "50% 50%",
+    //   rotate: -3.5,
+    //   scale:0.9
+    // }, {
+    //   rotate: 3.5,
+    //   scale:1,
+    //   ease: Elastic.easeInOut
+    // }, "=-0")
     .fromTo('#LeftArm', 0.5, {
       transformOrigin: "0% 50%",
       rotate: -7.5,
@@ -96,15 +124,17 @@ export class WoahComponent implements OnInit {
       x:0,
       ease: Elastic.easeInOut
     }, "=-0.5")
-    .fromTo('#Mask', 0.5, {
-      transformOrigin: "50% 50%",
-      y:-25,
-      scale:1.05,
-    }, {
-      y:0,
-      scale:1,
-      ease: Elastic.easeInOut
-    }, "=-0.5")    
+    // .fromTo('#Mask', 0.5, {
+    //   transformOrigin: "50% 50%",
+    //   y:-25,
+    //   scale:1.05,
+    //   rotate:-7.5,
+    // }, {
+    //   y:0,
+    //   scale:1,
+    //   rotate:7.5,
+    //   ease: Elastic.easeInOut
+    // }, "=-0.5")    
     .fromTo('#LeftArm',  0.5, {
       transformOrigin: "0% 50%",
       rotate: 7.5,
@@ -127,15 +157,15 @@ export class WoahComponent implements OnInit {
       x:25,
       ease: Elastic.easeInOut
     }, "=-0.5")
-    .fromTo('#Mask', 0.5, {
-      transformOrigin: "50% 50%",
-      y:0,
-      scale:1,
-    }, {
-      y:-25,
-      scale:1.05,
-      ease: Elastic.easeInOut
-    }, "=-0.5")
+    // .fromTo('#Mask', 0.5, {
+    //   transformOrigin: "50% 50%",
+    //   y:0,
+    //   scale:1,
+    // }, {
+    //   y:-25,
+    //   scale:1.05,
+    //   ease: Elastic.easeInOut
+    // }, "=-0.5")
     this.ArmAnimation.play();
   }
 
